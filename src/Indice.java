7278d15e860978cc9aca86027585aec88ce9fded
PabloMateosP <pmatpal0105@g.educaand.es>
2023-03-06 10:49:07 +0100

    Subida índice tema 6

---
+++ src/Indice.java
@@ -0,0 +1,21 @@
+public class Indice {
+    //1. Control y manejo de excepciones
+    //  a. Concepto
+    //  b. Jerarquía de excepciones
+    //  c. Miembros de una excepción
+    //  d. Manejo de excepciones
+    //    i. Captura
+    //    ii. Propagación
+    //    iii. Lanzamiento
+    //    iv. Excepciones de Usuario
+    //  e. Buenas prácticas
+    //2. Recursividad
+    //  a. Concepto
+    //  b. Utilización
+    //  c. Tipos de Recursividad
+    //  d. Ventajas e inconvenientes
+    //3. Complejidad algorítimica
+    //4. Algoritmia
+    //  a. Algoritmo voraz
+    //  b. Divide y vencerás
+}
