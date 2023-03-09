36da5205c5ab616a5ab27cfc044405d21af8c6e3
PabloMateosP <pmatpal0105@g.educaand.es>
2023-03-09 13:25:27 +0100

    Subida conceptos primera clase y ejemplos

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -1,12 +1,13 @@
 package Ejercicios;
 
 import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 //Meter un número en una lista
 public class Ejercicio1 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         List<Integer> lista = new ArrayList<>();
         lista.add(1);
         lista.add(2);
@@ -17,12 +18,19 @@ public class Ejercicio1 {
         lista.add(7);
 
         Integer nuevoNumero = 6;
-        añadirNumero(lista, nuevoNumero);
-    }
-    static void añadirNumero(List <Integer> lista, Integer nuevoNumero){
-        if (lista.contains(nuevoNumero)) {
-            throw new IOError("La lista ya contiene el número");
+        try {
+            añadirNumero(lista, nuevoNumero);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+    }
+    static void añadirNumero(List <Integer> lista, Integer nuevoNumero) throws IOException {
+        if (lista.contains(nuevoNumero)) {
+            throw new IOException("La lista ya contiene el número");
+        } else {
+            lista.add(nuevoNumero);
+            System.out.println("Nuevo número añadido");
         }
     }
 }
