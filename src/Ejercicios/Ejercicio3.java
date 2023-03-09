41489b672fa7ea99c1c7e679741ebe5061dd9f7f
PabloMateosP <pmatpal0105@g.educaand.es>
2023-03-09 13:52:46 +0100

    Subida Ejercicio3

---
+++ src/Ejercicios/Ejercicio3.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 300;
+        cociente(a, b);
+    }
+    static void cociente (int a, int b){
+        int c = b/a;
+        System.out.println(c);
+    }
+}
